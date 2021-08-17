37e8205e18fec6e1f7aeab2b540fb4ba6ceaf1a2
K3v1nW <lwcarvalho08@gmail.com>
2021-08-17 19:05:13 -0300

    Primeira Interface Grafica

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 #Calculadora Simples
+from tkinter import *
+from tkinter import ttk
 
-numero1 = 0
-numero2 = 0
-resultado = 0
-operacao = ''
+janela = Tk ()
+janela.title('Calculadora')
+janela.geometry("235x318")
 
-numero1 = int(input('Digite o primeiro valor: '))
-operacao = input('Digite a operação: ')
-numero2 = int(input('Digite o segundo valor: '))
+frame_tela = Frame(janela, width=235, height=50)
+frame_tela.grid(row=0, column=0)
+janela.mainloop()
 
-if operacao == '+':
-    resultado = numero1 + numero2
-elif operacao == '-':
-    resultado = numero1 - numero2
-elif operacao == '/':
-    resultado = numero1 / numero2
-elif operacao == '*':
-    resultado = numero1 * numero2
-else:
-    resultado = 'Opreação Inválida'
 
-print(str(numero1) + ' ' + str(operacao) +  ' ' + str(numero2) + ' = ' + str(resultado))
+# numero1 = 0
+# numero2 = 0
+# resultado = 0
+# operacao = ''
+
+# numero1 = int(input('Digite o primeiro valor: '))
+# operacao = input('Digite a operação: ')
+# numero2 = int(input('Digite o segundo valor: '))
+
+# if operacao == '+':
+#     resultado = numero1 + numero2
+# elif operacao == '-':
+#     resultado = numero1 - numero2
+# elif operacao == '/':
+#     resultado = numero1 / numero2
+# elif operacao == '*':
+#     resultado = numero1 * numero2
+# else:
+#     resultado = 'Opreação Inválida'
+
+# print(str(numero1) + ' ' + str(operacao) +  ' ' + str(numero2) + ' = ' + str(resultado))
        
